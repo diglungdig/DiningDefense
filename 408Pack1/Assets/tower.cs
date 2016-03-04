43dbe6707fe38b4edc51567b08f564397804a941
AtbTheGreat <annatiffanybenjamin@gmail.com>
2016-03-04 12:49:41 -0500

    tower changes
    Help me Wei you're my only hope

---
+++ 408Pack1/Assets/tower.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tower : MonoBehaviour {
+
+	// Use this for initialization
+	
+	
+	
+	
+	void Start () {
+	}
+	
+	
+	
+	void OnTriggerEnter(Collider sth){
+		
+		
+		if (sth.gameObject.thisType == "icecream"){
+			Vector2 dir = sth.transform.position - this.transform.position;
+			
+			//shoot in that direction
+			
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+	void OnTriggerExit(Collider sth){
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
