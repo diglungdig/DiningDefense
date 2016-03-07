9326a1277a686c34fec5af29aa84b7eb63827b0c
aalmazmi <aalmazmi@purdue.edu>
2016-03-07 17:06:23 -0500

    Removed Hand

--- 408Pack1/Assets/Script/cursorChanger.cs
+++ 408Pack1/Assets/Script/cursorChanger.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class cursorChanger : MonoBehaviour {
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
+	public Vector2 hotSpot = Vector2.zero;
 
         
     // Use this for initialization
     void Start () {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+		Cursor.SetCursor(cursorTexture, new Vector2(70,-4), cursorMode);
     }
 }
